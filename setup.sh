46f7c96c4a792431b887b7c9c50f4eeec46d903a
silviodosreis <silvioreis@combatfire.eng.br>
2024-10-17 20:13:54 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"prof.massaki@gmail.com\"\n\
+email = \"silvioreis@combatfire.eng.br\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
